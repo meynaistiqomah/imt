9f1471b5b30a377c3fa8af806ccf4d21d4d51009
viatree <viathree01@gmail.com>
2020-01-15 14:54:24 +0700

    penambahan test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+    
+    public static void main(String[] args) {
+        System.out.println("welcome");
+        System.out.println("to");
+        System.out.println("the");
+        System.out.println("System");
+    }
+    
+}
